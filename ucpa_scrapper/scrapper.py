f2230bd936432386a931fcc28f85f0c43142927a
shahar-ke <shaharke.dev@gmail.com>
2019-10-24 17:06:31 +0300

    think basic is ready
    Signed-off-by: shahar-ke <shaharke.dev@gmail.com>

--- ucpa_scrapper/scrapper.py
+++ ucpa_scrapper/scrapper.py
@@ -14,27 +14,27 @@ def download(url):
 
 
 EXPECTED = {
-    'chamonix':0,
-    'argentiere':0,
-    'chalet-les-pelerins':0,
-    'Chalet Pelerins':0,
-    'flaine':0,
-    'la-plagne':0,
-    'La Plagne':0,
-    'les-arcs':0,
-    'Les Arcs':0,
-    'les-contamines':0,
-    'Les Contamines':0,
-    'les-deux-alpes':0,
-    'Les Deux Alpes':0,
-    'serre-chevalier':0,
-    'Serre Chevalier':0,
-    'Tignes':0,
-    "val-d'isere":0,
-    "Val d'Isere":0,
-    'val-thorens':0,
-    "Val Thorens":0,
-    'special':0
+    'chamonix': 0,
+    'argentiere': 0,
+    'chalet-les-pelerins': 0,
+    'Chalet Pelerins': 0,
+    'flaine': 0,
+    'la-plagne': 0,
+    'La Plagne': 0,
+    'les-arcs': 0,
+    'Les Arcs': 0,
+    'les-contamines': 0,
+    'Les Contamines': 0,
+    'les-deux-alpes': 0,
+    'Les Deux Alpes': 0,
+    'serre-chevalier': 0,
+    'Serre Chevalier': 0,
+    'Tignes': 0,
+    "val-d'isere": 0,
+    "Val d'Isere": 0,
+    'val-thorens': 0,
+    "Val Thorens": 0,
+    'special': 0
 }
 
 
@@ -47,25 +47,24 @@ def get_password():
 def send_mail(email_msg):
     target_mail = 'deep_flame@yahoo.com'
     import smtplib
-    fromMy = target_mail
+    from_mail = target_mail
     # fun-fact: from is a keyword in python, you can't use it as variable, did anyone check if this code even works?
     to = target_mail
     subj = 'ucpa'
     date = datetime.datetime.now()
     message_text = email_msg
-
-    msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (fromMy, to, subj, date, message_text)
-
+    msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (from_mail, to, subj, date, message_text)
     username = str(target_mail)
     password = get_password()
     server = smtplib.SMTP("smtp.mail.yahoo.com", 587)
     server.starttls()
     server.login(username, password)
-    server.sendmail(fromMy, to, msg)
+    server.sendmail(from_mail, to, msg)
     server.quit()
 
 
 def main():
+    email_msg = list()
     try:
         url = "https://www.action-outdoors.co.uk/winter/deals/special-offers"
         page = download(url)
@@ -81,7 +80,6 @@ def main():
             for key in sorted(EXPECTED.keys()):
                 if key.lower() in line:
                     found[key] += 1
-        email_msg = list()
         for key in sorted(EXPECTED.keys()):
             if found[key] != EXPECTED[key]:
                 email_msg.append("%s, expected:%d, found:%d" % (key, EXPECTED[key], found[key]))
